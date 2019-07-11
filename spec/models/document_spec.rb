1c94ada2b1eb28e4393938363fb85c0ea0d95628
Javi Martín <javim@elretirao.net>
2019-07-11 13:38:21 +0200

    Reduce number of tests

--- spec/models/document_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-describe Document do
-
-  it_behaves_like "document validations", "budget_investment_document"
-  it_behaves_like "document validations", "proposal_document"
-
-end
